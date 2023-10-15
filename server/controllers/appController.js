143fbc70afa1480653fefd893a10d8cbc092a14e
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-10-15 09:54:26 -0700

    Added Auth middleware and generateOTP controller Fnx

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -2,6 +2,7 @@ import UserModel from "../model/User.model.js"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import ENV from '../config.js'
+import otpGenerator from 'otp-generator'
 
 /** middleware for verify user */
 export async function verifyUser(req, res, next){
@@ -155,12 +156,14 @@ export async function getUser(req, res){
  */
 export async function updateUser(req, res){
     try {
-        const id = req.query.id;
-        if (id){
+        // const id = req.query.id;
+        const { userId } = req.user;
+
+        if (userId){
             const body = req.body;
 
         // Update the data
-        UserModel.updateOne({ _id: id }, body, (err, data)=>{
+        UserModel.updateOne({ _id: userId }, body, (err, data)=>{
             if (err) throw err;
             return res.status(201).send({ msg: "Record Updated Successfully...!" })
         })
@@ -172,14 +175,22 @@ export async function updateUser(req, res){
     }
 }
 
+
 /** GET: http://localhost:8080/api/generateOTP */
 export async function generateOTP(req, res){
-    res.json('generateOTP route')
+    req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets:false, specialChars: false })
+    res.status(201).send({ code: req.app.locals.OTP })
 }
 
 /** GET: http://localhost:8080/api/verifyOTP */
 export async function verifyOTP(req, res){
-    res.json('verifyOTP route')
+    const { code } = req.query;
+    if(parseInt(req.app.locals.OTP) === parseInt(code)){
+        req.app.locals.OTP = null; //reset the OTP value
+        req.app.locals.resetSession = true //start the seesion for reset password
+        return res.status(201).send({ msg: "Verify Successfully!" })
+    }
+    return res.status(400).send({ error: "Invalid OTTP" })
 }
 
 
--- server/router/route.js
+++ server/router/route.js
@@ -3,6 +3,7 @@ const router = Router();
 
 /** Import all router controllers */
 import * as controller from '../controllers/appController.js'
+import Auth, { localVariables } from "../middleware/auth.js";
 
 /** POST Methods  */
 router.route('/register').post(controller.register); // Register User 
@@ -13,12 +14,12 @@ router.route('/login').post(controller.verifyUser, controller.login); // login t
 
 /**  GET Methods */
 router.route('/user/:username').get(controller.getUser); // Get user with username provided...
-router.route('/generateOTP').get(controller.generateOTP); // Generate random OTP
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP); // Generate random OTP
 router.route('/verifyOTP').get(controller.verifyOTP); // Verify generated OTP
 router.route('/createResetSession').get(controller.createResetSession); // Reset all the variables
 
 /** PUT Methods */
-router.route('/updateUser').put(controller.updateUser); // Update user details
+router.route('/updateUser').put(Auth, controller.updateUser); // Update user details
 router.route('/resetPassword').put(controller.resetPassword); // reset password
 
 export default router;
