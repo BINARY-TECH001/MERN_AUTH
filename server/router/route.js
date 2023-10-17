2a58aa42eb6874097d8c936b3c135e067c7b3d3a
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-10-16 19:31:38 -0700

    Started the client axios requests

--- server/router/route.js
+++ server/router/route.js
@@ -9,14 +9,14 @@ import { registerMail } from '../controllers/mailer.js'
 /** POST Methods  */
 router.route('/register').post(controller.register); // Register User 
 router.route('/registerMail').post( registerMail ); // send the email
-router.route('/authenticate').post(); // authenticate User
+router.route('/authenticate').post(controller.verifyUser, (req, res) => res.end()); // authenticate User
 router.route('/login').post(controller.verifyUser, controller.login); // login to app
 
 
 /**  GET Methods */
 router.route('/user/:username').get(controller.getUser); // Get user with username provided...
 router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP); // Generate random OTP
-router.route('/verifyOTP').get(controller.verifyOTP); // Verify generated OTP
+router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP); // Verify generated OTP
 router.route('/createResetSession').get(controller.createResetSession); // Reset all the variables
 
 /** PUT Methods */
